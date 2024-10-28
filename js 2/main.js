8b063c1114162acde2ef5d35768101c44bc11f8e
Leyla Muradzade <lvmuradzade55@gmail.com>
2024-10-28 15:36:56 +0400

    Add files via upload

---
+++ js 2/main.js
@@ -0,0 +1,41 @@
+// 1
+// let num1 = 5;
+// for (let i = 1; i <= num1; i++) {
+//  let star = '*'.repeat(i);
+//    console.log(star); 
+// }
+
+
+// 2
+// let num1 = 5;
+// for (let i = num1; i >= 1; i--) {
+//   let star = '*'.repeat(i);
+//     console.log(star);
+// }
+
+
+// 3
+// let name1 = prompt("adinizi yazin")
+// let name2 = prompt("soyadinizi yazin")
+// let age1 = prompt("dogum ilinizi yazin")
+// console.log(name1  +  name2  +  "siz"  +  age1  +  'cu ilde anadan olmusunuz');
+
+
+// 4
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 5 == 0) {
+//         console.log(i);
+//   }
+// }
+
+
+// 5
+// let correctUsername = "Anar";
+// let correctPassword = "2023";
+// let username = prompt("istifadeci adi");
+// let password = prompt("parol");
+// if (username == correctUsername && password == correctPassword) {
+//      console.log("Xoş gelmisiniz");
+// } else {
+//      console.log("İstifadeci adi ve ya parol yanlisdir");
+// }
